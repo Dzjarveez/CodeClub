10698736a09495d7629975b8a8d1392cb99c67e7
Dzjarveez <igorsautkin@gmail.com>
2023-10-16 18:00:14 +0300

    add scroll on click menu links

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const header = document.querySelector(".header");
 const whiteLogo = document.querySelector(".white-logo"); 
 const blackLogo = document.querySelector(".black-logo"); 
 const menuLinks = document.querySelectorAll(".menu__link"); 
+const menuSubLinks = document.querySelectorAll(".menu__sub-link"); 
 const menuSubList = document.querySelector(".menu__sub-list"); 
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu");
@@ -64,3 +65,58 @@ menuDarkSide.addEventListener("click", (event) => {
     document.body.classList.remove("_lock");
     menuDarkSide.classList.remove("_active");
 });
+
+// click scroll
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            console.log("1");
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+           
+            if (iconMenu.classList.contains("_active")) {
+                menuBody.classList.remove("_menu");
+                iconMenu.classList.remove("_active");
+                document.body.classList.remove("_lock");
+                menuDarkSide.classList.remove("_active");
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        };
+    };
+};
+if (menuSubLinks.length > 0) {
+    console.log(1);
+    menuSubLinks.forEach(menuSubLink => {
+        menuSubLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuSubLink = e.target;
+        if (menuSubLink.dataset.goto && document.querySelector(menuSubLink.dataset.goto)){
+            console.log("1");
+            const gotoBlock = document.querySelector(menuSubLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+            
+            menuBody.classList.remove("_menu");
+            iconMenu.classList.remove("_active");
+            document.body.classList.remove("_lock");
+            menuDarkSide.classList.remove("_active");
+            
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        };
+    };
+};
